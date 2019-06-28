bf05347e51c69ff5ed81336ce6fa4735afed478a
Lars den Bakker <larsdenbakker@gmail.com>
2019-06-28 21:01:22 +0200

    Take base href into account when resolving relative urls

--- src/common.js
+++ src/common.js
@@ -4,7 +4,14 @@ const envGlobal = hasSelf ? self : global;
 export { envGlobal as global };
 
 export let baseUrl;
-if (typeof location !== 'undefined') {
+
+if (typeof document !== 'undefined') {
+  const baseEl = document.querySelector('base[href]');
+  if (baseEl)
+    baseUrl = baseEl.href;
+}
+
+if (!baseUrl && typeof location !== 'undefined') {
   baseUrl = location.href.split('#')[0].split('?')[0];
   const lastSepIndex = baseUrl.lastIndexOf('/');
   if (lastSepIndex !== -1)
@@ -99,10 +106,10 @@ export function resolveIfNotPlainOrUrl (relUrl, parentUrl) {
 
 /*
  * Import maps implementation
- * 
+ *
  * To make lookups fast we pre-resolve the entire import map
  * and then match based on backtracked hash lookups
- * 
+ *
  */
 
 function resolveUrl (relUrl, parentUrl) {
