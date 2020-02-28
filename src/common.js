27e7c6d1bc690672962f5d98c8da32dd24782efd
Guy Bedford <guybedford@gmail.com>
2020-02-28 03:14:51 +0200

    typo

--- src/common.js
+++ src/common.js
@@ -199,7 +199,7 @@ export async function resolveAndComposeImportMap (json, baseUrl, parentMap) {
 
   if (json.depcache)
     for (let d in json.depcache) {
-      const resolvedDepcache = resolveUrl(s, baseUrl);
+      const resolvedDepcache = resolveUrl(d, baseUrl);
       outMap.depcache[resolvedDepcache] = json.depcache[d];
     }
 
