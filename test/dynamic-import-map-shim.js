e0820198b43d38ce64d48343b36e8986ded9eee2
Vladimir Guguiev <wizardzloy@gmail.com>
2021-05-14 15:17:08 +0200

    Add delayed insert-import to the dynamic-import-map-shim test

--- test/dynamic-import-map-shim.js
+++ test/dynamic-import-map-shim.js
@@ -1,20 +1,16 @@
 main();
 async function main() {
-    // Append a dynamic import map containing resolution for "react-dom".
-    document.body.appendChild(Object.assign(document.createElement('script'), {
-        type: 'importmap-shim',
-        innerHTML: JSON.stringify({
-            "imports": {
-                "react-dom": "https://ga.jspm.io/npm:react-dom@17.0.2/dev.index.js"
-            },
-            "scopes": {
-                "https://ga.jspm.io/": {
-                    "scheduler": "https://ga.jspm.io/npm:scheduler@0.20.2/dev.index.js",
-                    "scheduler/tracing": "https://ga.jspm.io/npm:scheduler@0.20.2/dev.tracing.js"
-                }
+    insertDynamicImportMap({
+        "imports": {
+            "react-dom": "https://ga.jspm.io/npm:react-dom@17.0.2/dev.index.js"
+        },
+        "scopes": {
+            "https://ga.jspm.io/": {
+                "scheduler": "https://ga.jspm.io/npm:scheduler@0.20.2/dev.index.js",
+                "scheduler/tracing": "https://ga.jspm.io/npm:scheduler@0.20.2/dev.tracing.js"
             }
-        })
-    }));
+        }
+    });
 
     const reactStart = performance.now();
     const [React, ReactDOM] = await Promise.all([
@@ -25,4 +21,22 @@ async function main() {
 
     console.log(`React and ReactDOM loaded in ${(reactEnd - reactStart).toFixed(2)}ms`);
     console.log({ React, ReactDOM });
+
+    setTimeout(async () => {
+        insertDynamicImportMap({
+            "imports": {
+                "lodash": "https://ga.jspm.io/npm:lodash-es@4.17.21/lodash.js",
+            }
+        })
+
+        const lodash = await importShim("lodash")
+        console.log({ lodash })
+    }, 1000)
+}
+
+function insertDynamicImportMap(importMap) {
+    document.body.appendChild(Object.assign(document.createElement('script'), {
+        type: 'importmap-shim',
+        innerHTML: JSON.stringify(importMap)
+    }));
 }
