67bfadd7b4819e815d1cf1e94c243c5eb43159c0
Guy Bedford <guybedford@gmail.com>
2021-08-20 13:35:01 -0700

    fixup matrix

--- src/es-module-shims.js
+++ src/es-module-shims.js
@@ -33,7 +33,6 @@ async function loadAll (load, seen) {
     load.n = load.d.some(dep => dep.n);
 }
 
-
 let importMap = { imports: {}, scopes: {} };
 let importMapSrcOrLazy = false;
 let importMapPromise = resolvedPromise;
