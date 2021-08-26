e5846392f300a4f95a02c407080e0e1c815691b9
Guy Bedford <guybedford@gmail.com>
2021-08-26 07:45:00 -0700

    Update src/common.js

--- src/common.js
+++ src/common.js
@@ -67,6 +67,7 @@ export const featureDetectionPromise = Promise.all([
     const iframe = document.createElement('iframe');
     iframe.style.display = 'none';
     document.body.appendChild(iframe);
+    // we use document.write here because eg Weixin built-in browser doesn't support setting srcdoc
     iframe.contentWindow.document.write(`<script type=importmap>{"imports":{"x":"data:text/javascript,"}}<${''}/script><script>import('x').then(()=>1,()=>0).then(v=>parent._$s(v))<${''}/script>`);
   })
 ]);
