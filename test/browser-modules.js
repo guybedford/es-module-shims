12a4844bb18a042310acc9c3d546d9ad2c34aa4e
Guy Bedford <guybedford@gmail.com>
2021-09-06 23:53:55 -0700

    begin bisection

--- test/browser-modules.js
+++ test/browser-modules.js
@@ -2,7 +2,7 @@ const edge = !!navigator.userAgent.match(/Edge\/\d\d\.\d+$/);
 
 self.baseURL = location.href.substr(0, location.href.lastIndexOf('/') + 1);
 
-suite('Basic loading tests', () => {
+suite.skip('Basic loading tests', () => {
   test('Static load order and domcontentloaded and ready state', async function () {
     window.onerror = () => {};
     await new Promise(resolve => {
