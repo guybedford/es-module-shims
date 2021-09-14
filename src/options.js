1cc971565fadbd671536dfeb7a16c4d69fbe8d54
Guy Bedford <guybedford@gmail.com>
2021-09-13 21:39:12 -0700

    fixup typo

--- src/options.js
+++ src/options.js
@@ -9,14 +9,13 @@ export const resolveHook = shimMode && esmsInitOptions.resolve;
 
 export const skip = esmsInitOptions.skip ? new RegExp(esmsInitOptions.skip) : /^https:\/\/(cdn\.skypack\.dev|jspm\.dev)\//;
 
-export let nonce = esmInitOptions.nonce;
+export let nonce = esmsInitOptions.nonce;
 
 if (!nonce) {
   const nonceElement = document.querySelector('script[nonce]');
   if (nonceElement)
     nonce = nonceElement.nonce;
 }
-  
 
 export const {
   fetchHook = fetch,
