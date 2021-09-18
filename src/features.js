38c88e68eb28d2d7204db4f4c63f01aba8f4c94b
Guy Bedford <guybedford@gmail.com>
2021-09-17 23:18:12 -0700

    fixup feature detections

--- src/features.js
+++ src/features.js
@@ -1,6 +1,6 @@
 import { dynamicImport, supportsDynamicImportCheck } from './dynamic-import.js';
 import { noop, createBlob } from './common.js';
-import { nonce } from './options.js';
+import { nonce, cssModulesEnabled, jsonModulesEnabled } from './options.js';
 
 // support browsers without dynamic import support (eg Firefox 6x)
 export let supportsJsonAssertions = false;
@@ -11,20 +11,27 @@ export let supportsImportMaps = false;
 
 export let supportsDynamicImport = false;
 
-export const featureDetectionPromise = Promise.resolve(supportsDynamicImportCheck).then(ok => !ok ? 0 : (supportsDynamicImport = true, Promise.all([
-  dynamicImport(createBlob('import"data:text/css,{}"assert{type:"css"}'), { errUrl: 'f1' }).then(() => supportsCssAssertions = true, noop),
-  dynamicImport(createBlob('import"data:text/json,{}"assert{type:"json"}'), { errUrl: 'f2', }).then(() => supportsJsonAssertions = true, noop),
-  dynamicImport(createBlob('import.meta'), { errUrl: 'f3' }).then(() => supportsImportMeta = true, noop),
-  new Promise(resolve => {
-    self._$s = v => {
-      document.body.removeChild(iframe);
-      delete self._$s;
-      resolve(v);
-    };
-    const iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
-    // we use document.write here because eg Weixin built-in browser doesn't support setting srcdoc
-    iframe.contentWindow.document.write(`<script type=importmap nonce="${nonce}">{"imports":{"x":"data:text/javascript,"}}<${''}/script><script nonce="${nonce}">import('x').then(()=>1,()=>0).then(v=>parent._$s(v))<${''}/script>`);
-  }).then(() => supportsImportMaps = true, noop)
-])), noop);
+export const featureDetectionPromise = Promise.resolve(supportsDynamicImportCheck).then(_supportsDynamicImport => {
+  if (!_supportsDynamicImport)
+    return;
+  supportsDynamicImport = true;
+
+  return Promise.all([
+    dynamicImport(createBlob('import.meta')).then(() => supportsImportMeta = true, noop).
+    cssModulesEnabled && dynamicImport(createBlob('import"data:text/css,{}"assert{type:"css"}')).then(() => supportsCssAssertions = true, noop),
+    jsonModulesEnabled && dynamicImport(createBlob('import"data:text/json,{}"assert{type:"json"}')).then(() => supportsJsonAssertions = true, noop),
+    new Promise(resolve => {
+      self._$s = v => {
+        document.body.removeChild(iframe);
+        if (v) supportsImportMaps = true;
+        delete self._$s;
+        resolve();
+      };
+      const iframe = document.createElement('iframe');
+      iframe.style.display = 'none';
+      document.body.appendChild(iframe);
+      // we use document.write here because eg Weixin built-in browser doesn't support setting srcdoc
+      iframe.contentWindow.document.write(`<script type=importmap nonce="${nonce}">{"imports":{"x":"data:text/javascript,"}}<${''}/script><script nonce="${nonce}">import('x').then(()=>1,()=>0).then(v=>parent._$s(v))<${''}/script>`);
+    })
+  ]);
+});
