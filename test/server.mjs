c1344d78bcb5622a76fc41ff990568885c3f7c45
Guy Bedford <guybedford@gmail.com>
2021-12-14 20:13:15 -0800

    fixup CI retry

--- test/server.mjs
+++ test/server.mjs
@@ -42,7 +42,7 @@ function setBrowserTimeout () {
     clearTimeout(browserTimeout);
   browserTimeout = setTimeout(() => {
     retry += 1;
-    if (retry >= 1) {
+    if (retry > 1) {
       console.log('No browser requests made to server for 10s, closing.');
       process.exit(failTimeout || process.env.CI_BROWSER ? 1 : 0);
     }
