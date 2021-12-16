239892bb34330410e4b4dc7bcd91daadd98a7507
Richard Macklin <1863540+rmacklin@users.noreply.github.com>
2021-12-15 22:12:31 -0800

    Fix typo and move comment about source map compatibility above the line it documents (#241)

--- src/es-module-shims.js
+++ src/es-module-shims.js
@@ -268,7 +268,6 @@ function resolveDeps (load, seen) {
     resolvedSource += source.slice(lastIndex);
   }
 
-  // ; and // trailer support added for Ruby 7 source maps compatibility
   let hasSourceURL = false;
   resolvedSource = resolvedSource.replace(sourceMapURLRegEx, (match, isMapping, url) => (hasSourceURL = !isMapping, match.replace(url, () => new URL(url, load.r))));
   if (!hasSourceURL)
@@ -278,6 +277,8 @@ function resolveDeps (load, seen) {
   load.S = undefined;
 }
 
+// ; and // trailer support added for Ruby on Rails 7 source maps compatibility
+// https://github.com/guybedford/es-module-shims/issues/228
 const sourceMapURLRegEx = /\n\/\/# source(Mapping)?URL=([^\n]+)\s*((;|\/\/[^#][^\n]*)\s*)*$/;
 
 const jsContentType = /^(text|application)\/(x-)?javascript(;|$)/;
