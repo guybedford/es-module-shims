3724aa1a06ac16b96397862fbad930de68c8d426
Guy Bedford <guybedford@gmail.com>
2022-04-03 12:16:17 +0200

    fix: bug where non-existing dynamic import would fail polyfill execution

--- src/es-module-shims.js
+++ src/es-module-shims.js
@@ -433,8 +433,10 @@ function getOrCreateLoad (url, fetchOpts, parent, source) {
   load.L = load.f.then(async () => {
     let childFetchOpts = fetchOpts;
     load.d = (await Promise.all(load.a[0].map(async ({ n, d }) => {
-      if (d >= 0 && !supportsDynamicImport || d === 2 && !supportsImportMeta)
+      if (d >= 0 && !supportsDynamicImport || d === 2 && !supportsImportMeta) {
         load.n = true;
+        return;
+      }
       if (!n) return;
       const { r, b } = await resolve(n, load.r || load.u);
       if (b && (!supportsImportMaps || importMapSrcOrLazy))
