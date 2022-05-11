256435d02e2576d5146d06145a385a204d430651
Iain Beeston <iainbeeston@users.noreply.github.com>
2022-05-11 18:40:12 +0100

    fix: add nonce to feature detection iframe (#290)

--- src/features.js
+++ src/features.js
@@ -28,6 +28,7 @@ export const featureDetectionPromise = Promise.resolve(supportsDynamicImportChec
       };
       const iframe = document.createElement('iframe');
       iframe.style.display = 'none';
+      iframe.setAttribute('nonce', nonce);
       iframe.srcdoc = `<script type=importmap nonce="${nonce}">{"imports":{"x":"data:text/javascript,"}}<${''}/script><script nonce="${nonce}">import('x').then(()=>1,()=>0).then(v=>parent._$s(v))<${''}/script>`;
       document.head.appendChild(iframe);
     })
